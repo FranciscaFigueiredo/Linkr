30a9f6e81f642fe7bd5c1e95fa0fbe1ae486212d
Kleyton <kleyton_vccamargo@hotmail.com>
2022-03-24 19:36:47 -0300

    fix: publish, token, session refresh, post

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,27 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import UserContext from '../../contexts/UserContext';
 import { getPosts } from '../../services/linkr.js';
 import { LinkSnippet } from '../LinkSnippet/index.js';
 import Loader from '../Loader.js';
 import { toastError } from '../toasts.js';
 import { Post, PostContent, PostsContainer, PostSidebar } from './styles.js';
 
-export default function Posts() {
+export default function Posts({refresh}) {
   const [posts, setPosts] = useState();
+  const { token } = useContext(UserContext);
 
   useEffect(() => {
-    getPosts()
+    getPosts(token || JSON.parse(sessionStorage.getItem('user')).token)
       .then((res) => setPosts(res.data))
       .catch(() => {
         toastError(
           'An error occured while trying to fetch the posts, please refresh the page'
         );
       });
-  }, []);
+  }, [refresh]);
 
   return posts ? (
     <PostsContainer>
       {posts.length > 0 ? (
-        posts.map((post) => {
+        posts.reverse().map((post) => {
           return (
             <Post key={post.id}>
               <PostSidebar>
--- src/pages/Timeline/Timeline.js
+++ src/pages/Timeline/Timeline.js
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import Header from '../../components/Header/Header.js';
 import Posts from '../../components/Posts/';
 import Publish from '../../components/Publish/index.js';
 import { TimelineContainer, TimelineParent } from './styles.js';
 
 export default function Timeline() {
+  const [refresh, setRefresh] = useState(true)
   return (
     <>
       <Header />
       <TimelineContainer>
         <TimelineParent>
           <span id='title'>timeline</span>
-          <Publish />
-          <Posts />
+          <Publish refresh={refresh} setRefresh={setRefresh} />
+          <Posts refresh={refresh} />
         </TimelineParent>
       </TimelineContainer>
     </>
