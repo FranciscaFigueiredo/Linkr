9671cfab77a0ffa6af51d2d61573dd8a2b1cd17e
Kleyton <kleyton_vccamargo@hotmail.com>
2022-03-26 22:10:38 -0300

    fix: /users

--- src/pages/UserTimeline/index.js
+++ src/pages/UserTimeline/index.js
@@ -1,21 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Header from "../../components/Header/Header"
 import Posts from "../../components/Posts"
 import Trending from "../../components/Trending"
+import { getUserById } from "../../services/linkr"
 import { TimelineContainer, TimelineParent } from "../Timeline/styles"
 
 
-export default function User(){
+export default function UserTimeline(){
     const { id } = useParams()
     const [name, setName] = useState('')
+    useEffect(()=>{
+        const promise = getUserById(id)
+        promise.then(res => {
+            console.log(res.data.username);
+            setName(res.data.username)
+        }).catch(err => console.log(err.response.message));
+    },[id])
     return(
         <>
             <Header />
             <TimelineContainer>
                 <TimelineParent>
                     <span id='title'>{`${name}'s posts`}</span>
-                    <Posts id={id} setName={setName} />
+                    <Posts id={id} />
                 </TimelineParent>
                 <Trending />
             </TimelineContainer>
