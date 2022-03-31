e4ba6fbec98f81186e2b8617b3180cd3a75d9f54
Felipe Sakuma <fycorsak@gmail.com>
2022-03-31 20:42:23 -0300

    pre-merge

--- src/components/Post/Comments/PostComments/Comment.js
+++ src/components/Post/Comments/PostComments/Comment.js
@@ -1,13 +1,18 @@
 import { CommentContainer, CommentContent } from './styles.js';
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, post }) {
+  console.log({ comment });
+  console.log({ post });
+
+  let userInfo;
+  if (comment.userId === post.userId) userInfo = "• post's author";
   return (
     <CommentContainer>
       <img src={comment.userPic} alt='' />
       <CommentContent>
         <div id='userContainer'>
           <span id='commentUsername'>{comment.username}</span>
-          <span id='userInfo'>• following</span>
+          <span id='userInfo'>{userInfo}</span>
           {/* TODO replace static value with logic */}
         </div>
         <span id='commentText'>{comment.text}</span>
