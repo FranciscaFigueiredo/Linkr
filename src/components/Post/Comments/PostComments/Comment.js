15713afd9132b279a6c8164312ae30cee5f265a5
Felipe Sakuma <fycorsak@gmail.com>
2022-03-31 21:16:43 -0300

    fix: user comment info + style of no posts span

--- src/components/Post/Comments/PostComments/Comment.js
+++ src/components/Post/Comments/PostComments/Comment.js
@@ -1,19 +1,33 @@
 import { CommentContainer, CommentContent } from './styles.js';
+import { useNavigate } from 'react-router-dom';
 
 export default function Comment({ comment, post }) {
-  console.log({ comment });
-  console.log({ post });
+  const navigate = useNavigate();
 
-  let userInfo;
+  const follows = JSON.parse(localStorage.getItem('follows')).map(
+    (followed) => followed.followed_id
+  );
+
+  let userInfo = '';
   if (comment.userId === post.userId) userInfo = "• post's author";
+
+  if (follows.find((followedId) => followedId === comment.userId))
+    userInfo += '• following';
+
   return (
     <CommentContainer>
       <img src={comment.userPic} alt='' />
       <CommentContent>
         <div id='userContainer'>
-          <span id='commentUsername'>{comment.username}</span>
+          <span
+            id='commentUsername'
+            onClick={() => {
+              navigate(`/users/${comment.userId}`);
+            }}
+          >
+            {comment.username}
+          </span>
           <span id='userInfo'>{userInfo}</span>
-          {/* TODO replace static value with logic */}
         </div>
         <span id='commentText'>{comment.text}</span>
       </CommentContent>
